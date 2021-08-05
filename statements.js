5141166e12c688109ad1d298c07c5a776d9b9599
Sndrahel <sandra.helander@hotmail.com>
2021-08-05 08:41:42 +0000

    Add solution code to statements.js

--- statements.js
+++ statements.js
@@ -36,3 +36,35 @@ $("a:last");
 
 // Select all header elements (h1, h2, h3, h4, h5, h6)
 $(":header");
+
+/**
+ * The set of statements that are executed in the browser console to try out
+ * jQuery selectors
+ */
+
+// Get all img elements on the page
+$("img");
+
+// Get all elements with a class of `card_image`
+$(".card_image");
+
+// Get the footer by its ID
+$('#my_footer');
+
+// All paragrapgh elements in the `footer`
+$("footer p");
+
+// Get all header elements (h1-h6)
+$(":header");
+
+// Get first element on the page
+$(":first");
+
+// Get the last `div` on the page
+$("div:last")
+
+// Get the last `img` on the page
+$("img:last");
+
+// Get all elements that have an attribute of `href`
+$("[href]")
